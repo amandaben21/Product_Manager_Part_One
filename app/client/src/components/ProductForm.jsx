4d920643a5ff26603c2dd3e77d3447ff56895700
Amanda Contreras <amandaben21@aol.com>
2023-03-23 23:34:11 -0400

    The client folder was not opening on github, so i created an 'app' folder

---
+++ app/client/src/components/ProductForm.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import axios from "axios";
+
+const ProductForm = (props) => {
+
+    const {products, setProducts} = props;
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState("");
+
+    const createProduct = (e) => {
+        e.preventDefault();
+
+        axios.post("http://localhost:8000/api/products", {
+            title,
+            price,
+            description
+        })
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                console.log(res.data.product);
+                setProducts([...products, res.data.product]);
+
+                setTitle("");
+                setPrice(0);
+                setDescription("");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <div>
+            <h1>Product Manager</h1>
+            <form onSubmit={createProduct}>
+                <div className="form-fields">
+                    <label>Title</label>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                </div>
+                <div className="form-fields">
+                    <label>Price</label>
+                    <input type="number" value={price} onChange={(e) => setPrice(e.target.value)}/>
+                </div>
+                <div className="form-fields">
+                    <label>Description</label>
+                    <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                </div>
+                <input type="submit" value="Create"/>
+            </form>
+        </div>
+    )
+}
+
+export default ProductForm;
